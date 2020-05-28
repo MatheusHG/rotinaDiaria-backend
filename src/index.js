a4a13c4b085d4fac072911421b59fc35ca327402
MatheusHG <56003521+MatheusHG@users.noreply.github.com>
2020-05-27 21:15:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ app.use(bodyParser.urlencoded({ extended: false }))
 require('./controllers/authController')(app) 
 require('./controllers/activityController')(app)
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
