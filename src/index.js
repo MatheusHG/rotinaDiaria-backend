d00604b5e5f1da8324d789d0d2c5a5a5e7f35804
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-06-17 14:55:37 -0300

    update cors

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ require('./controllers/activityController')(app)
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials': true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Connection, authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
