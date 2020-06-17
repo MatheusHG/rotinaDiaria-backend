c2dd14c3bccf895ae88d397ddc519bd51075d701
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-06-17 14:07:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const cors = require('cors')
 const app = express() 
 
 app.use(cors())
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: false })) 
+
+require('./controllers/authController')(app) 
+require('./controllers/activityController')(app)
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -12,12 +17,5 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
-
-app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({ extended: false })) 
-
-require('./controllers/authController')(app) 
-require('./controllers/activityController')(app)
-
 
 app.listen(process.env.PORT || 3000)
