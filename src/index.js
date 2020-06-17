c50317b93a2960e35de2a97fbccf69597a8c2ef4
Matheus Henrique <matheus.oliveira@ccc.ufcg.edu.br>
2020-06-17 14:59:03 -0300

    update cors

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ require('./controllers/authController')(app)
 require('./controllers/activityController')(app)
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://tarefasbackend.herokuapp.com/');
-    res.header('Access-Control-Allow-Credentials': true);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Connection, authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
